54ae64b5061612d3c3c221429a9a6ce9eb8038af
jamescollison <james.collison@my.jcu.edu.au>
2019-05-12 15:40:45 +1000

    rough alternative header

--- the-coffee-can/the-coffee-can/header.php
+++ the-coffee-can/the-coffee-can/header.php
@@ -25,26 +25,31 @@
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'the-coffee-can' ); ?></a>
 
 	<header id="masthead" class="site-header">
-		<div class="site-branding">
 
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			endif;
-			$the_coffee_can_description = get_bloginfo( 'description', 'display' );
-			if ( $the_coffee_can_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $the_coffee_can_description; /* WPCS: xss ok. */ ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
 
+<!--		<div class="site-branding">-->
+<!---->
+<!--			--><?php
+//
+//			if ( is_front_page() && is_home() ) :
+//				?>
+<!--				<h1 class="site-title"><a href="--><?php //echo esc_url( home_url( '/' ) ); ?><!--" rel="home">--><?php //bloginfo( 'name' ); ?><!--</a></h1>-->
+<!--				--><?php
+//			else :
+//				?>
+<!--				<h1 class="site-title"><a href="--><?php //echo esc_url( home_url( '/' ) ); ?><!--" rel="home">--><?php //bloginfo( 'name' ); ?><!--</a></h1>-->
+<!--				--><?php
+//			endif;
+//			$the_coffee_can_description = get_bloginfo( 'description', 'display' );
+//			if ( $the_coffee_can_description || is_customize_preview() ) :
+//				?>
+<!--				<p class="site-description">--><?php //echo $the_coffee_can_description; /* WPCS: xss ok. */ ?><!--</p>-->
+<!--			--><?php //endif; ?>
+<!--		</div>-->
+        <div class="header-menu">
+        <?php
+        the_custom_logo();
+        ?>
 		<nav id="site-navigation" class="main-navigation">
 			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'the-coffee-can' ); ?></button>
 			<?php
@@ -52,11 +57,15 @@
 				'theme_location' => 'menu-1',
 				'menu_id'        => 'primary-menu',
 			) );
+
 			?>
 		</nav><!-- #site-navigation -->
+        </div>
         <div class="header-image">
-        <?php the_header_image_tag(); ?>
+            <?php the_header_image_tag(); ?>
         </div>
 	</header><!-- #masthead -->
 
 	<div id="content" class="site-content">
+
+
