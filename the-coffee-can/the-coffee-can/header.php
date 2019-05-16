91e343f4859bce15ef400fbd5c5d28baaf72b7e9
jamescollison <james.collison@my.jcu.edu.au>
2019-05-16 10:25:44 +1000

    clean code

--- the-coffee-can/the-coffee-can/header.php
+++ the-coffee-can/the-coffee-can/header.php
@@ -23,29 +23,7 @@
 <body <?php body_class(); ?>>
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'the-coffee-can' ); ?></a>
-
 	<header id="masthead" class="site-header">
-
-
-<!--		<div class="site-branding">-->
-<!---->
-<!--			--><?php
-//
-//			if ( is_front_page() && is_home() ) :
-//				?>
-<!--				<h1 class="site-title"><a href="--><?php //echo esc_url( home_url( '/' ) ); ?><!--" rel="home">--><?php //bloginfo( 'name' ); ?><!--</a></h1>-->
-<!--				--><?php
-//			else :
-//				?>
-<!--				<h1 class="site-title"><a href="--><?php //echo esc_url( home_url( '/' ) ); ?><!--" rel="home">--><?php //bloginfo( 'name' ); ?><!--</a></h1>-->
-<!--				--><?php
-//			endif;
-//			$the_coffee_can_description = get_bloginfo( 'description', 'display' );
-//			if ( $the_coffee_can_description || is_customize_preview() ) :
-//				?>
-<!--				<p class="site-description">--><?php //echo $the_coffee_can_description; /* WPCS: xss ok. */ ?><!--</p>-->
-<!--			--><?php //endif; ?>
-<!--		</div>-->
         <div class="header-menu">
         <?php
         the_custom_logo();
